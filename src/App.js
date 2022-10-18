79665b8ba9a8f0e326aa2d4663cef736e9531885
TahielGiordana <tahielgiordana@gmail.com>
2022-10-18 15:04:10 -0300

    Se puede reproducir con el teclado

--- src/App.js
+++ src/App.js
@@ -5,55 +5,46 @@ const clips = [
     clipId: "Heater 1",
     clipSrc: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3",
     keyCode: "Q",
-    keyTrigger: 0,
   },
   {
     clipId: "Heater 2",
     clipSrc: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
     keyCode: "W",
-    keyTrigger: 0,
   },
   {
     clipId: "Heater 3",
     clipSrc: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3",
     keyCode: "E",
-    keyTrigger: 0,
   },
   {
     clipId: "Heater 4",
     clipSrc: "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3",
     keyCode: "A",
-    keyTrigger: 0,
   },
   {
     clipId: "Clap",
     clipSrc: "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3",
     keyCode: "S",
-    keyTrigger: 0,
   },
   {
     clipId: "Open-HH",
     clipSrc: "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3",
     keyCode: "D",
-    keyTrigger: 0,
   },
   {
     clipId: "Kick-n'-Hat",
     clipSrc: "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3",
     keyCode: "Z",
-    keyTrigger: 0,
   },
   {
     clipId: "Kick",
     clipSrc: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3",
     keyCode: "X",
-    keyTrigger: 0,
   },
   {
     clipId: "Closed-HH",
     clipSrc: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
     keyCode: "C",
-    keyTrigger: 0,
   },
 ];
 
@@ -80,6 +71,7 @@ class App extends React.Component {
             clipSrc={clip.clipSrc}
             keyCode={clip.keyCode}
             updateDisplay={this.updateDisplay}
+            key={clip.clipId}
           />
         ))}
       </div>
@@ -91,6 +83,14 @@ class DrumPad extends React.Component {
   constructor(props) {
     super(props);
     this.playSound = this.playSound.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener("keydown", (event) => {
+      if (event.key.toLowerCase() == this.props.keyCode.toLowerCase()) {
+        this.playSound();
+      }
+    });
   }
 
   playSound() {
